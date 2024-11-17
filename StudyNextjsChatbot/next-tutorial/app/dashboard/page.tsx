0b6dec431adada0e43ba7624872d339a518f55c2
LINKER <temp2temp@daum.net>
2024-11-17 18:42:10 +0900

    [LEARN]: 다이나믹 라우트, Api 패치

--- StudyNextjsChatbot/next-tutorial/app/dashboard/page.tsx
+++ StudyNextjsChatbot/next-tutorial/app/dashboard/page.tsx
@@ -1,4 +1,8 @@
-export default function DashboardPage() {
+export default async function DashboardPage() {
+    // path를 사용하기에 async를 추가한다.
+    const response = await fetch("http://localhost:3000/api/test");
+    const data = await response.json();
+    console.log("data",data);
     return (
         <main>
         <h1>Dashboard</h1>
